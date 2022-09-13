e8331484e0e7b83be26ad0d56d48660374c3412a
libing <444537935@qq.com>
2022-09-13 22:53:53 +0800

    增加签到历史记录相关接口

---
+++ src/main/java/com/bing/monkey/haagendzs/entity/SignDateRange.java
@@ -0,0 +1,16 @@
+package com.bing.monkey.haagendzs.entity;
+
+import lombok.Data;
+
+/**
+ * Created by Intellij IDEA.
+ * User:  Bing
+ * Date:  2022/9/13
+ */
+@Data
+public class SignDateRange {
+
+    private String startSingDate;
+    private String endSingDate;
+
+}
